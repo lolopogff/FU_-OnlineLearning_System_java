da63ccf21c6790f6d1d34aa733c0e18d0ab63ad6
Vadim Manakov <manakov_vadim2004@mail.ru>
2025-10-11 16:45:11 +0300

    roles logic

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -4,12 +4,10 @@ import com.example.demo.entity.Course;
 import com.example.demo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,7 +15,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class CourseController {
 
     @Autowired
-    private CourseService courseService;
+    CourseService courseService;
+
 
     @RequestMapping("/")
     public String listOfCourses(Model model) {
@@ -34,9 +33,9 @@ public class CourseController {
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String saveCourse(@ModelAttribute("course") Course course) {
-        courseService.save(course);
-        return "redirect:/courses/list";
+    public String saveCourse(@ModelAttribute("course") Course course, Authentication auth) {
+        courseService.save(course, auth);
+        return "redirect:/courses/";
     }
 
     @RequestMapping("/edit/{id}")
@@ -47,9 +46,19 @@ public class CourseController {
         return mav;
     }
 
+
     @RequestMapping("/delete/{id}")
-    public String deleteCourse(@PathVariable("id") Long id) {
-        courseService.deleteCourse(id);
-        return "redirect:/courses/list";
+    @PreAuthorize("hasAnyRole('TEACHER', 'ADMIN')")
+    public String deleteCourse(@PathVariable("id") Long id, Authentication authentication) {
+        courseService.deleteCourse(id, authentication);
+        return "redirect:/courses/";
     }
+
+
+    @GetMapping("/myCourses")
+    public String myCourses(Model model, Authentication authentication) {
+        model.addAttribute("my_courses", courseService.getCourseByUserId(authentication));
+        return "course/myCourses";
+    }
+
 }
